c34ba07701f56b9412a1df8900775d8f7f319548
Steffen Remus <steremus@gmail.com>
2026-07-04 12:57:28 +0000

    add robots meta tags for pages

--- nextjs-app/src/app/layout.tsx
+++ nextjs-app/src/app/layout.tsx
@@ -106,6 +106,19 @@ export const metadata: Metadata = {
     address: false,
     telephone: false,
   },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 export default function RootLayout({
--- nextjs-app/src/app/private/home/page.tsx
+++ nextjs-app/src/app/private/home/page.tsx
@@ -3,6 +3,14 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Home",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      noimageindex: true,
+    },
+  },
 };
 
 export default function Home() {
