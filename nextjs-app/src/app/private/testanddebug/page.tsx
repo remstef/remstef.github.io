112e663492ef2e8499a2c9f465614ee5e30f7b29
Steffen Remus <steremus@gmail.com>
2026-07-04 14:59:13 +0200

    apply changes from itsme-again

--- nextjs-app/src/app/private/testanddebug/page.tsx
+++ nextjs-app/src/app/private/testanddebug/page.tsx
@@ -14,6 +14,14 @@ import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Hello World",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      noimageindex: true,
+    },
+  },
 };
 
 export default function TestAndDebug() {
