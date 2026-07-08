dca51908f6c2dff34604e783e925c1c3c3361a06
Steffen Remus <steremus@gmail.com>
2026-07-08 10:25:38 +0000

    move private route  to _private

--- nextjs-app/src/app/robots.ts
+++ nextjs-app/src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: "/_private/",
     },
     sitemap: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/sitemap.xml`,
   };
