b0976b50c6f087f129858a312b03f6984fc5c12c
Steffen Remus <steremus@gmail.com>
2026-07-10 09:15:15 +0000

    fix font issue firefox moboile

--- nextjs-app/src/components/user-agent-detector.tsx
+++ nextjs-app/src/components/user-agent-detector.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function UserAgentDetector(){
   // Add this to your layout or a client component
   useEffect(() => {
-    const isFirefox = navigator.userAgent.includes("Firefox");
+    const isFirefox = navigator.userAgent.includes("Moz");
     const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     
     if (isFirefox && isMobile) {
