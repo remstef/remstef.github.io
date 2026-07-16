95bdfee3e6f36c04b215540cc09a9a4938927250
Steffen Remus <steremus@gmail.com>
2026-07-16 19:08:47 +0000

    formatting

--- nextjs-app/src/components/navbar.tsx
+++ nextjs-app/src/components/navbar.tsx
@@ -63,13 +63,12 @@ export const NavigationBar: React.FunctionComponent<INavbarProps> = (props) => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              {" "}
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
+              />
             </svg>
           </summary>
           <ul
@@ -78,26 +77,25 @@ export const NavigationBar: React.FunctionComponent<INavbarProps> = (props) => {
           >
             {navItems.map((pageItem) => (
               <li key={pageItem.id}>
-                {currentPathIsActivePath(pageItem.hrefs) ? 
-                  <Link 
+                {currentPathIsActivePath(pageItem.hrefs) ? (
+                  <Link
                     role="button"
                     tabIndex={0}
                     className="btn btn-ghost underline"
-                    href={pageItem.hrefs[0]} 
-                    onClick={closeDetailsSummaryDropdown} 
-                    children={<h2>{pageItem.label}</h2>} 
+                    href={pageItem.hrefs[0]}
+                    onClick={closeDetailsSummaryDropdown}
+                    children={<h2>{pageItem.label}</h2>}
                   />
-                  
-                :
-                  <Link 
+                ) : (
+                  <Link
                     role="button"
                     tabIndex={0}
                     className="btn btn-ghost font-normal"
-                    href={pageItem.hrefs[0]} 
-                    onClick={closeDetailsSummaryDropdown} 
-                    children={<div>{pageItem.label}</div>} 
+                    href={pageItem.hrefs[0]}
+                    onClick={closeDetailsSummaryDropdown}
+                    children={<div>{pageItem.label}</div>}
                   />
-                }
+                )}
               </li>
             ))}
           </ul>
@@ -111,18 +109,31 @@ export const NavigationBar: React.FunctionComponent<INavbarProps> = (props) => {
         <ul className="menu menu-horizontal">
           {navItems.map((pageItem) => (
             <li key={pageItem.id}>
-              {currentPathIsActivePath(pageItem.hrefs) ? 
-                <Link className="px-2 btn btn-ghost underline" href={pageItem.hrefs[0]} children={<h2>{pageItem.label}</h2>} />
-              :
-                <Link className="px-2 btn btn-ghost font-normal" href={pageItem.hrefs[0]} children={<div>{pageItem.label}</div>} />
-              }
+              {currentPathIsActivePath(pageItem.hrefs) ? (
+                <Link
+                  className="px-2 btn btn-ghost underline"
+                  href={pageItem.hrefs[0]}
+                  children={<h2>{pageItem.label}</h2>}
+                />
+              ) : (
+                <Link
+                  className="px-2 btn btn-ghost font-normal"
+                  href={pageItem.hrefs[0]}
+                  children={<div>{pageItem.label}</div>}
+                />
+              )}
             </li>
           ))}
         </ul>
       </div>
       {/*   */}
       <div className="navbar-end">
-        <Link href="/" role="button" tabIndex={0} className="btn btn-ghost text-xl font-normal">
+        <Link
+          href="/"
+          role="button"
+          tabIndex={0}
+          className="btn btn-ghost text-xl font-normal"
+        >
           <h1>{theProtagonistsName}</h1>
         </Link>
       </div>
