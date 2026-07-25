ff91e4b243d5447593fadf40d205ff93fb9b4bca
Steffen Remus <steremus@gmail.com>
2026-07-25 16:57:45 +0000

    refactorings

--- nextjs-app/src/app/cvmd/_page.server.tsx
+++ nextjs-app/src/app/cvmd/_page.server.tsx
@@ -3,7 +3,7 @@ import { CvContent } from "./_page-content.client";
 // just a wrapper component, so that the client can receive the loaded md files
 export default async function CvPage() {
   // This runs at build time on the server
-  const { staticMarkdown } = await import("./_markdown-loader.server");
+  const { staticMarkdown } = await import("./_static-markdown-loader.server");
 
   return <CvContent md={staticMarkdown} />;
 }
